c61b5b6e6bcef84667dcacfd879fc38f479e6902
chleese <yuanyuan.li100@gmail.com>
2023-12-14 00:24:19 +0800

    update to new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo-icon.png";
+import name from "./Chleese.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='flex flex-center'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <img src={name} className='logo-title' alt='title' />
+        </div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
+      <div></div>
     </div>
   );
 }
