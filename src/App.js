aeafdd94ad511fb135bf9bd8345da39e3e398218
chleese <yuanyuan.li100@gmail.com>
2024-01-10 00:11:21 +0800

    Go To URL

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from "./logo-icon.png";
 import name from "./Chleese.png";
 import "./App.css";
 import Menu from "./Component/Menu.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./Component/About.tsx";
+import Home from "./Component/Home.tsx";
+import Projects from "./Component/Projects.tsx";
 function App() {
   return (
     <div className='App'>
@@ -10,16 +14,15 @@ function App() {
           <img src={logo} className='App-logo' alt='logo' />
           <img src={name} className='logo-title' alt='title' />
         </div>
-        <Menu />
-        {/* <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a> */}
       </header>
-      <div></div>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Component/Menu.tsx
+++ src/Component/Menu.tsx
@@ -1,29 +1,22 @@
 import React from "react";
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
-  const activeStyle = { color: "#424242" };
-  const [activeItem, setActiveItem] = useState<string | null>("home");
-
   return (
     <div className='menu'>
-      <ul>
-        <li
-          style={activeItem === "home" ? activeStyle : undefined}
-          onClick={() => setActiveItem("home")}>
-          首页
-        </li>
-        <li
-          style={activeItem === "project" ? activeStyle : undefined}
-          onClick={() => setActiveItem("project")}>
-          项目经历
-        </li>
-        <li
-          style={activeItem === "me" ? activeStyle : undefined}
-          onClick={() => setActiveItem("me")}>
-          关于我
-        </li>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/projects'>Projects</Link>
+          </li>
+          <li>
+            <Link to='/about'>About Me</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
